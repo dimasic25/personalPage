903e05c837b347fcca6460875059bdaf0b91e9ab
Dmitry <dimonbubyr@mail.ru>
2021-09-16 14:14:02 +0300

    доделана декстопная версия

---
+++ js/script.js
@@ -0,0 +1,13 @@
+new Swiper('.image-slider', {
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+
+        clickable: true,
+    },
+});
