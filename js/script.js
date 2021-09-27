25149e984aff63ef89aa82651d1b555e7394f5af
dimasic25 <78664325+dimasic25@users.noreply.github.com>
2021-09-27 20:26:26 +0300

    рефактор слайдера 2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let slider = new Swiper('.swiper', {
     loop: true,
 
     slidesPerView: 2,
-    spaceBetween: 20,
+    spaceBetween: 0,
 
     navigation: {
         nextEl: '.swiper-button-next',
